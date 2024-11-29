fac23cffa6547857b4d846ed80ab6d9d91b7cda5
akas25n <akas25n@gmail.com>
2024-11-29 06:50:11 +0100

    add third error

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,17 @@ import './App.css'
 
 import * as Sentry from "@sentry/react";
 
+let release = `react-sentry-monitoring@1.0.0`
+
 Sentry.init({
   dsn: "https://51d5cdfdcfcdd9effea58e7b8292ca44@o4508324719165440.ingest.de.sentry.io/4508360413282384",
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
   ],
+  release: release,
   // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions for performance monitoring
 });
 
 function App() {
@@ -26,9 +24,9 @@ function App() {
   return (
     <>
       <h1>Hello Sentry</h1>
-      <button onClick={() => {throw new Error("This is the first error!");}}>Break the world</button>
+      <button onClick={() => {throw new Error("This is third error!");}}>Break the world</button>
     </>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
